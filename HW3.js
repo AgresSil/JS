ef52bec35f6faa86844ccdbd7c7cd2ff02839609
EVGENY SILIN <69542356+AgresSil@users.noreply.github.com>
2021-12-21 17:53:33 +0300

    Update HW3.js

--- HW3.js
+++ HW3.js
@@ -11,3 +11,22 @@ for (let i = 2; i <= n; i++) {
 }
 
 //##############################
+
+const basket = [
+  ['Телефон', 38000, 3],
+  ['Ноутбук', 40000, 1],
+];
+
+
+function countBasketPrice(basket) {
+  let totalPrice = 0;
+  for (let i = 0; i < basket.length; i++) {
+    totalPrice += basket[i][1] * basket[i][2];
+  }
+  return totalPrice;
+}
+
+
+console.log(countBasketPrice(basket));
+
+// ********************************
